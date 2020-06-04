497f9dae7c2df2268794ff6d40ad43a76cd03acd
ChaseKnowlden <knowlden887@gmail.com>
2020-06-04 15:35:16 -0400

    fix return statement

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function mooncakeSays(message) {
-    let mooncake = '(O.o)';
+  let mooncake = '(O.o)';
 
-    if ( !message ) mooncake;
+  if ( !message ) mooncake;
+
+  return `${mooncake} ${message}`;
 }
 
 module.exports.mooncakeSays = mooncakeSays;
